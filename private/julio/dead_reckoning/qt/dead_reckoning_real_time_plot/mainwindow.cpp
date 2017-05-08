8e2e1898d9f9a37467a35f5c89bf393e15b8093d
Julio C. Laptop-Work <tachidok@gmail.com>
2017-05-08 18:55:24 -0500

    Dead reckoning Telit. Adding Qt interface. Plotting in real time.
    -- Plots in real time (as information is read).
    -- QCustomPlot to promote widgets and allow plotting.

--- private/julio/dead_reckoning/qt/dead_reckoning_real_time_plot/mainwindow.cpp
+++ private/julio/dead_reckoning/qt/dead_reckoning_real_time_plot/mainwindow.cpp
@@ -7,9 +7,126 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // ---------------------------------------------------
+    // Add options to plot options
+    Plots_options.append("Raw Gyro X");
+    Plots_options.append("Raw Gyro Y");
+    Plots_options.append("Raw Gyro Z");
+    Plots_options.append("Raw Acc X");
+    Plots_options.append("Raw Acc Y");
+    Plots_options.append("Raw Acc Z");
+    Plots_options.append("Rotated Gyro X");
+    Plots_options.append("Rotated Gyro Y");
+    Plots_options.append("Rotated Gyro Z");
+    Plots_options.append("Rotated Acc X");
+    Plots_options.append("Rotated Acc Y");
+    Plots_options.append("Rotated Acc Z");
+    Plots_options.append("Filtered Gyro X");
+    Plots_options.append("Filtered Gyro Y");
+    Plots_options.append("Filtered Gyro Z");
+    Plots_options.append("Filtered Acc X");
+    Plots_options.append("Filtered Acc Y");
+    Plots_options.append("Filtered Acc Z");
+    Plots_options.append("Aligned Gyro X (in time)");
+    Plots_options.append("Aligned Gyro Y (in time)");
+    Plots_options.append("Aligned Gyro Z (in time)");
+    Plots_options.append("Aligned Acc X (in time)");
+    Plots_options.append("Aligned Acc Y (in time)");
+    Plots_options.append("Aligned Acc Z (in time)");
+    Plots_options.append("Euler angle PHI from Gyro");
+    Plots_options.append("Euler angle THETA from Gyro");
+    Plots_options.append("Euler angle PSI from Gyro");
+    Plots_options.append("Euler angle PHI from Acc");
+    Plots_options.append("Euler angle THETA from Acc");
+    Plots_options.append("Euler angle PHI (fused)");
+    Plots_options.append("Euler angle THETA (fused)");
+    Plots_options.append("Euler angle PSI (fused)");
+    Plots_options.append("Gravity (in X-body frame)");
+    Plots_options.append("Gravity (in Y-body frame)");
+    Plots_options.append("Gravity (in Z-body frame)");
+    Plots_options.append("Linear Acc X (in body frame)");
+    Plots_options.append("Linear Acc Y (in body frame)");
+    Plots_options.append("Linear Acc Z (in body frame)");
+    Plots_options.append("Inertial Acc X (in inertial frame)");
+    Plots_options.append("Inertial Acc Y (in inertial frame)");
+    Plots_options.append("Inertial Acc Z (in inertial frame)");
+    Plots_options.append("Velocity X (in body frame)");
+    Plots_options.append("Velocity Y (in body frame)");
+    Plots_options.append("Velocity North (in inertial frame)");
+    Plots_options.append("Velocity East (in inertial frame)");
+    Plots_options.append("Position X (in body frame)");
+    Plots_options.append("Position Y (in body frame)");
+    Plots_options.append("Position X (in inertial frame)");
+    Plots_options.append("Position Y (in inertial frame)");
+
+    // Add options to combo boxs
+    ui->cmb1->addItems(Plots_options);
+    ui->cmb2->addItems(Plots_options);
+    ui->cmb3->addItems(Plots_options);
+    ui->cmb4->addItems(Plots_options);
+    ui->cmb5->addItems(Plots_options);
+    ui->cmb6->addItems(Plots_options);
+    ui->cmb7->addItems(Plots_options);
+    ui->cmb8->addItems(Plots_options);
+    ui->cmb9->addItems(Plots_options);
+    ui->cmb10->addItems(Plots_options);
+    ui->cmb11->addItems(Plots_options);
+    ui->cmb12->addItems(Plots_options);
+
+    // ---------------------------------------------------
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::plot_raw_gyro_x(const double gyro_x)
+{
+    const unsigned range_in_x = 10000;
+
+    // Increase the global counter
+    Global_counter_for_plot++;
+
+    // Add the current mouse coordinates to the data to plot
+    Trajectory_coordinates[0].push_back(X_trajectory);
+    Trajectory_coordinates[1].push_back(Y_trajectory);
+
+    // Add the current Kalman coordinate to the data to plot
+    Kalman_coordinates[0].push_back(X_Kalman);
+    Kalman_coordinates[1].push_back(Y_Kalman);
+
+    QVector<double> time_data(Global_counter_for_plot);
+
+    for (unsigned i = 0; i < Global_counter_for_plot; i++)
+    {time_data[i] = i;}
+
+    // ---------------------------------------------------------------
+    // X PLOT
+    // ---------------------------------------------------------------
+    // Assign data to plot
+    // ---------------------------------------------------------------
+    // Mouse (GREEN)
+    //ui->wdg_plot->graph(0)->setLineStyle(QCPGraph::lsNone);
+    ui->wdg_plot_x->graph(0)->setPen(QPen(QColor(0,255,0)));
+    ui->wdg_plot_x->graph(0)->setData(time_data, Trajectory_coordinates[0]);
+    // ---------------------------------------------------------------
+    // Kalman (BLUE)
+    ui->wdg_plot_x->graph(1)->setPen(QPen(QColor(0,0,255)));
+    ui->wdg_plot_x->graph(1)->setData(time_data, Kalman_coordinates[0]);
+
+    // Assign labels to axis    ui->wdg_plot_x->xAxis->setLabel("time");
+    ui->wdg_plot_x->yAxis->setLabel("x-position");
+
+    // Set range
+    ui->wdg_plot_x->xAxis->setRange(0, range_in_x);
+    ui->wdg_plot_x->yAxis->setRange(0, x_max);
+
+    ui->wdg_plot_x->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+    ui->wdg_plot_x->replot();
+
 }
--- private/julio/dead_reckoning/qt/dead_reckoning_real_time_plot/mainwindow.h
+++ private/julio/dead_reckoning/qt/dead_reckoning_real_time_plot/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QString>
+#include <QStringList>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +20,116 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    // ------------------------------------------------------
+    // List of plots for each widget
+    QStringList Plots_options;
+
+    // ------------------------------------------------------
+    // Plot methods (each widget calls one of these to plot its data)
+    void plot_raw_gyro_x();
+    void plot_raw_gyro_y();
+    void plot_raw_gyro_z();
+    void plot_raw_acc_x();
+    void plot_raw_acc_y();
+    void plot_raw_acc_z();
+    void plot_rotated_gyro_x();
+    void plot_rotated_gyro_y();
+    void plot_rotated_gyro_z();
+    void plot_rotated_acc_x();
+    void plot_rotated_acc_y();
+    void plot_rotated_acc_z();
+    void plot_filtered_gyro_x();
+    void plot_filtered_gyro_y();
+    void plot_filtered_gyro_z();
+    void plot_filtered_acc_x();
+    void plot_filtered_acc_y();
+    void plot_filtered_acc_z();
+    void plot_aligned_gyro_x();
+    void plot_aligned_gyro_y();
+    void plot_aligned_gyro_z();
+    void plot_aligned_acc_x();
+    void plot_aligned_acc_y();
+    void plot_aligned_acc_z();
+    void plot_euler_angle_phi_gyro();
+    void plot_euler_angle_theta_gyro();
+    void plot_euler_angle_psi_gyro();
+    void plot_euler_angle_phi_acc();
+    void plot_euler_angle_theta_acc();
+    void plot_euler_angle_phi_fused();
+    void plot_euler_angle_theta_fused();
+    void plot_euler_angle_psi_fused();
+    void plot_gravity_x_body_frame();
+    void plot_gravity_y_body_frame();
+    void plot_gravity_z_body_frame();
+    void plot_linear_acc_x();
+    void plot_linear_acc_y();
+    void plot_linear_acc_z();
+    void plot_inertial_acc_x();
+    void plot_inertial_acc_y();
+    void plot_inertial_acc_z();
+    void plot_velocity_x();
+    void plot_velocity_y();
+    void plot_velocity_north();
+    void plot_velocity_east();
+    void plot_position_x_body_frame();
+    void plot_position_y_body_frame();
+    void plot_position_x_inertial_frame();
+    void plot_position_y_inertial_frame();
+
+    // ------------------------------------------------------
+    // Data structures to store the computed values
+    QVector<double> Time;
+    QVector<double> Raw_gyro_x;
+    QVector<double> Raw_gyro_y;
+    QVector<double> Raw_gyro_z;
+    QVector<double> Raw_acc_x;
+    QVector<double> Raw_acc_y;
+    QVector<double> Raw_acc_z;
+    QVector<double> Rotated_gyro_x;
+    QVector<double> Rotated_gyro_y;
+    QVector<double> Rotated_gyro_z;
+    QVector<double> Rotated_acc_x;
+    QVector<double> Rotated_acc_y;
+    QVector<double> Rotated_acc_z;
+    QVector<double> Filtered_gyro_x;
+    QVector<double> Filtered_gyro_y;
+    QVector<double> Filtered_gyro_z;
+    QVector<double> Filtered_acc_x;
+    QVector<double> Filtered_acc_y;
+    QVector<double> Filtered_acc_z;
+    QVector<double> Aligned_gyro_x;
+    QVector<double> Aligned_gyro_y;
+    QVector<double> Aligned_gyro_z;
+    QVector<double> Aligned_acc_x;
+    QVector<double> Aligned_acc_y;
+    QVector<double> Aligned_acc_z;
+    QVector<double> Euler_angle_phi_gyro;
+    QVector<double> Euler_angle_theta_gyro;
+    QVector<double> Euler_angle_psi_gyro;
+    QVector<double> Euler_angle_phi_acc;
+    QVector<double> Euler_angle_theta_acc;
+    QVector<double> Euler_angle_phi_fused;
+    QVector<double> Euler_angle_theta_fused;
+    QVector<double> Euler_angle_psi_fused;
+    QVector<double> Gravity_x_body_frame;
+    QVector<double> Gravity_y_body_frame;
+    QVector<double> Gravity_z_body_frame;
+    QVector<double> Linear_acc_x;
+    QVector<double> Linear_acc_y;
+    QVector<double> Linear_acc_z;
+    QVector<double> Inertial_acc_x;
+    QVector<double> Inertial_acc_y;
+    QVector<double> Inertial_acc_z;
+    QVector<double> Velocity_x;
+    QVector<double> Velocity_y;
+    QVector<double> Velocity_north;
+    QVector<double> Velocity_east;
+    QVector<double> Position_x_body_frame;
+    QVector<double> Position_y_body_frame;
+    QVector<double> Position_x_inertial_frame;
+    QVector<double> Position_y_inertial_frame;
+
 };
 
 #endif // MAINWINDOW_H
