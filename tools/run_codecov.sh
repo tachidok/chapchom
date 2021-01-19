ae9cbd76ab314b328adb82b3e3baba6f911388b1
Julio C. - ASUS 14 <tachidok@gmail.com>
2021-01-18 20:04:45 -0600

    Add script ./tools/run_codecov.sh to generate code reports

--- tools/run_codecov.sh
+++ tools/run_codecov.sh
@@ -5,10 +5,14 @@
 #====================================================================
 # Folder to scan for coverage info
 working_folder=$(pwd)
+build_dir=build
 # File for coverage information
 lcov_output_file=coverage.info
-# Output folder for full report
-lcov_output_folder=lcov_output_folder
+# Folder to store code report
+code_report_dir=code_report
+# Folder to store html report output
+code_report_html_dir=html
+
 # The name of the library
 lib_name=SciCell++
 
@@ -28,31 +32,115 @@ echo ""
 #====================================================================
 echo ""
 echo "============================================================= "
-echo "I am going to run the coverage tools (lcov)"
+echo "Running coverage tools (lcov --capture) ... "
 echo "============================================================= "
 echo ""
-lcov --capture --directory $working_folder --output-file $lcov_output_file
-lcov --remove $lcov_output_file '/usr/*' --output-file $lcov_output_file
-lcov --list $lcov_output_file
+if ! lcov --capture --directory $working_folder/$build_dir --output-file $lcov_output_file ; then
+    echo ""
+    echo ""
+    echo ""
+    echo "========================================================= "
+    echo "[FAIL] lcov --capture"
+    echo "========================================================= "
+    echo ""
+    exit 1
+fi
 
 #====================================================================
-# Update the report to CodeCov.io
+# Remove kernel information from report and output in the specified
+# file
+# ===================================================================
+echo ""
+echo "============================================================= "
+echo "Removing kernel information (lcov --remove)"
+echo "============================================================= "
+echo ""
+if ! lcov --remove $lcov_output_file '/usr/*' --output-file $lcov_output_file ; then
+    echo ""
+    echo ""
+    echo ""
+    echo "========================================================= "
+    echo "[FAIL] lcov --remove"
+    echo "========================================================= "
+    echo ""
+    exit 1
+fi
+
 #====================================================================
+# Listitng file on stdout
+# ===================================================================
 echo ""
 echo "============================================================= "
-echo "I am going to update the report to CodeCov.io"
+echo "Listing file on stdout (lcov --list)"
 echo "============================================================= "
 echo ""
-bash <(curl -s https://codecov.io/bash) -f $lcov_output_file || echo "Codecov did not collect coverage reports"
+if ! lcov --list $lcov_output_file; then
+    echo ""
+    echo ""
+    echo ""
+    echo "========================================================= "
+    echo "[FAIL] lcov --list"
+    echo "========================================================= "
+    echo ""
+    exit 1
+fi
+
+#====================================================================
+# Move report to $code_report_dir
+# ===================================================================
+echo ""
+echo "Moving report to '$code_report_dir'"
+echo ""
+#====================================================================
+# Check whether the code_report folder exist
+#====================================================================
+if (test -d  $code_report_dir); then
+    rm -r $code_report_dir/*
+else
+    mkdir $code_report_dir
+fi
+
+if ! mv $lcov_output_file ./$code_report_dir; then
+    echo ""
+    echo ""
+    echo ""
+    echo "========================================================= "
+    echo "[FAIL] 'mv $lcov_output_file ./$code_report_dir'"
+    echo "========================================================= "
+    echo ""
+    exit 1
+fi
 
 #====================================================================
 # Generate report using the 
 #====================================================================
-#genhtml $lcov_output_file --output-directory $lcov_output_folder
+echo ""
+echo "Generate HTML code report in './$code_report_dir/$code_report_html_dir'"
+echo ""
+if ! genhtml ./$code_report_dir/$lcov_output_file --output-directory ./$code_report_dir/$code_report_html_dir ; then
+    echo ""
+    echo ""
+    echo ""
+    echo "========================================================= "
+    echo "[FAIL] 'genhtml ./$code_report_dir/$lcov_output_file --output-directory ./$code_report_dir/$code_report_html_dir'"
+    echo "========================================================= "
+    echo ""
+    exit 1
+fi
+
+#====================================================================
+# Update the report to CodeCov.io
+#====================================================================
+#echo ""
+#echo "============================================================= "
+#echo "I am going to update the report to CodeCov.io"
+#echo "============================================================= "
+#echo ""
+#bash <(curl -s https://codecov.io/bash) -f $lcov_output_file || echo "Codecov did not collect coverage reports"
 
 echo ""
 echo "[DONE] Coverage report script"
 echo ""
 
 # Done
-exit 1
+exit 0
